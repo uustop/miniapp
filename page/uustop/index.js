4be0323aafa3cedd3ad5011acb470b7129638cd9
uustop <yth3412@163.com>
2017-03-29 17:26:09 +0800

    init mobile page

---
+++ page/uustop/index.js
@@ -0,0 +1,26 @@
+Page({
+  data: {
+    list: [
+      {
+        id: 'view',
+        name: '测试小程序',
+        open: false,
+        pages: ['input']
+      }
+    ]
+  },
+  kindToggle: function (e) {
+    var id = e.currentTarget.id, list = this.data.list;
+    for (var i = 0, len = list.length; i < len; ++i) {
+      if (list[i].id == id) {
+        list[i].open = !list[i].open
+      } else {
+        list[i].open = false
+      }
+    }
+    this.setData({
+      list: list
+    });
+  }
+})
+
